84e28bcae625a9c2e5575d1a0cd5ea3b6d52da43
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2023-03-02 17:33:14 -0300

    Refatoramento de testes

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -5,8 +5,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import { Model } from 'sequelize';
-import TeamModel from '../database/models/TeamModel';
-import TeamService from '../api/services/TeamService';
+import Team from '../database/models/TeamModel';
 
 import { Response } from 'superagent';
 
@@ -14,24 +13,13 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const mockTeam: TeamModel[] = [new TeamModel({
-  id: 1,
-  teamName: 'Flamengo',
-})]
-
 describe('01 - Testa Teams', () => {
-  beforeEach(() => {
-    sinon.stub(Model, 'findAll').resolves(mockTeam);
-  })
-
-  afterEach(() => {
-    (TeamModel.findAll as sinon.SinonStub).restore();
-  })
-
   it('para o retorno de getAll', async () => {
-    const teamService = new TeamService();
-    const data = await teamService.getAll();
+    const mockTeam: Team[] = [{ id: 1, teamName: 'Avaí/Kindermann'}] as Team[];
+    sinon.stub(Model, 'findAll').resolves(mockTeam);
+    const result = await chai.request(app).get('/teams');
 
-    chai.expect(data).to.be.equal(mockTeam);
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.deep.equal(mockTeam);
   });
 });
